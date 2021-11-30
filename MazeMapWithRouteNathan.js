e3c19a6f16e9e7eab614eca292788c1356514f34
Nathan Dawson <nathadawson123@hotmail.co.uk>
2021-11-30 14:18:06 +0000

    Add files via upload

--- MazeMapWithRouteNathan.js
+++ MazeMapWithRouteNathan.js
@@ -40,24 +40,27 @@ map.on("load", () => {
   set_route(start, end);
 
   locationController.setState('follow'); 
-  const watchId = navigator.geolocation.watchPosition(position => {
+
+});
+
+const watchId = navigator.geolocation.watchPosition(position => {
       
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+  var latitude  = position.coords.latitude;
+  var longitude = position.coords.longitude;
 
-    locationController.updateLocationData({
-      lngLat: {
-        lng: longitude,
-        lat: latitude
-      }
-    });
-
-    if(trigger) {
-      trigger = false;
-      set_route({ lngLat: { lng: longitude, lat: latitude }, zLevel: map.zLevel }, end);
+  locationController.updateLocationData({
+    lngLat: {
+      lng: longitude,
+      lat: latitude
     }
   });
+
+  if(trigger) {
+    trigger = false;
+    set_route({ lngLat: { lng: longitude, lat: latitude }, zLevel: map.zLevel }, end);
+  }
 });
+
 
 function resetTrigger(){
   setTimeout(()=>{
